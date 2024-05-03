2cbf286505ce1b56317e95145aed0995db27d357
Snox11 <kritanshyaulak@gmail.com>
2024-05-03 17:38:13 +0545

    Completed Swaptemp

--- Assignments/Swaptemp.java
+++ Assignments/Swaptemp.java
@@ -4,9 +4,18 @@ public class Swaptemp {
     public static void main(String[] args) {
         //Swaping usign temp
         Scanner scan=new Scanner(System.in);
-        int a;
-
-        int b;
+        int a=1;
+        System.out.println("First number a ="+a);
+        int b=0;
+System.out.println("Second number b ="+b);
+//now swaping  using temp variable
+int temp;
+temp=a;
+a=b;
+b=temp;
+//now they are swapped
+System.out.println("now a ="+a);
+System.out.println("now b="+b);
 
     }
 
