db91e82b62236f98b85263589b5e4e6ccc1d566b
Snox11 <kritanshyaulak@gmail.com>
2024-05-03 18:10:46 +0545

    Incomplete now do after file handling

--- Assignments1/Asci.java
+++ Assignments1/Asci.java
@@ -1,4 +1,4 @@
-package Assignments;
+package Assignments1;
 import java.util.Scanner;
 public class Asci {
     public static void main(String[] args) {
--- Assignments1/Swaptemp.java
+++ Assignments1/Swaptemp.java
@@ -1,4 +1,4 @@
-package Assignments;
+package Assignments1;
 import java.util.Scanner;
 public class Swaptemp {
     public static void main(String[] args) {
