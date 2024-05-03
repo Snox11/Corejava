90462741ee8ef0ef23c2a4c7132bcf7ccbed7725
Snox11 <kritanshyaulak@gmail.com>
2024-05-03 17:30:37 +0545

    2nd upload

---
+++ Assignments/Swaptemp.java
@@ -0,0 +1,13 @@
+package Assignments;
+import java.util.Scanner;
+public class Swaptemp {
+    public static void main(String[] args) {
+        //Swaping usign temp
+        Scanner scan=new Scanner(System.in);
+        int a;
+
+        int b;
+
+    }
+
+}
