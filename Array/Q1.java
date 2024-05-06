bfea8cc3f09d8857693650aa8c9e578c730be659
Snox11 <kritanshyaulak@gmail.com>
2024-05-06 17:15:40 +0545

    Adding new array examples or questions

---
+++ Array/Q1.java
@@ -0,0 +1,20 @@
+package Array;
+import java.util.Scanner;
+//WAP to store and print the avg price of 10 books
+public class Q1 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+int price[]=new int[10];
+for(int i=0;i<10;i++){
+    System.out.println("Enter the price of the book");
+    price[i]=scan.nextInt();
+}
+for(int i=0;i<10;i++){
+    System.out.println("The price are "+price[i]);
+}
+
+scan.close();        
+    }
+    
+
+}
---
+++ Array/Q5.java
@@ -0,0 +1,33 @@
+package Array;
+import java.util.Scanner;
+public class Q5 {
+public static void main(String[] args) {
+    Scanner scan=new Scanner(System.in);
+    //creating array for storing subjects
+    String Subjects[]=new String[5];
+    // Taking Subject names form user
+for(int i=0;i<5;i++){
+    System.out.println("Enter the subject name");
+    Subjects[i]=scan.next();
+}
+//creating variable for marks 
+int marks[]=new int[5];
+//taking marks in each subject
+for(int i=0;i<5;i++)
+{
+    System.out.println("Enter the marks in " + Subjects[i]);
+    marks[i]=scan.nextInt();
+} 
+//calculating total
+int sum=0;
+for(int i=0;i<5;i++)
+{
+    sum=sum+marks[i];
+}
+System.out.println("The total is " + sum);
+//calculating percent 
+float percent=(sum*100)/500;
+System.out.println("The percentage is " + percent);
+scan.close();
+}
+}
