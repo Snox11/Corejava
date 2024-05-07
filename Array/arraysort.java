8e9a8af9c7368735a019c3cbfb490fafd1075f00
Snox11 <kritanshyaulak@gmail.com>
2024-05-07 18:13:32 +0545

    Array sort and methods

---
+++ Array/arraysort.java
@@ -0,0 +1,19 @@
+package Array;
+
+import java.util.Arrays;
+
+public class arraysort {
+public static void main(String[] args) {
+    
+    int[] a={1,78,3,4,5};
+    Arrays.sort(a);
+    System.out.println(Arrays.toString(a));
+    for(int i=a.length-1;i>=0;i--){
+        System.out.print(a[i]+" ");
+
+    }
+
+}
+    
+
+}
