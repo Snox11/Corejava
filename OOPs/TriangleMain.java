9bb6aef94879027417b4e5a444aeeeb414d5010d
Snox11 <kritanshyaulak@gmail.com>
2024-05-08 18:34:41 +0545

    not sure about constructors and parameters

---
+++ OOPs/TriangleMain.java
@@ -0,0 +1,14 @@
+package OOPs;
+
+public class TriangleMain {
+    public static void main(String[] args) {
+        int a=3;
+        int b=4;
+        int c=5;
+ Triangle d=new Triangle();
+double e=d.perimeter(a, b, c);
+System.out.println("The perimeter is "+e);
+System.out.println("The area is "+d.area(a, b, c, e));
+    }
+
+}
