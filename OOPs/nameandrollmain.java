16ad67754a4ee88b3bf9a43049147986f87d5e63
Snox11 <kritanshyaulak@gmail.com>
2024-05-09 15:54:55 +0545

    Just test files Scanner Learned in Classes

---
+++ OOPs/nameandrollmain.java
@@ -0,0 +1,11 @@
+package OOPs;
+
+public class nameandrollmain {
+    public static void main(String[] args) {
+        nameandroll a=new nameandroll();
+      a.get_name();
+       a.get_roll();
+        a.print();
+    }
+
+}
