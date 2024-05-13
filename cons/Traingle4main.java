158e63e81cc67d7d573ebc73f3ca96e591a43fd1
Snox11 <kritanshyaulak@gmail.com>
2024-05-13 16:21:00 +0545

    changed package name

--- cons/Traingle4main.java
+++
@@ -1,9 +0,0 @@
-package cons;
-
-public class Traingle4main {
-    public static void main(String[] args) {
-        Triangle4 a=new Triangle4();
-        a.Triangle();
-    }
-
-}
--- cons/Traingle5main.java
+++
@@ -1,9 +0,0 @@
-package cons;
-
-public class Traingle5main {
-    public static void main(String[] args) {
-        Traingle5 a=new Traingle5(3, 4, 5);
-        a.Traingle();
-    }
-
-}
