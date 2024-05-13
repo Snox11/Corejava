35b12a304682607383d3bfbec7b0d07630176c69
Snox11 <kritanshyaulak@gmail.com>
2024-05-13 17:14:12 +0545

    LEarned about statics

---
+++ Method/ChangeStaticValue.java
@@ -0,0 +1,28 @@
+package Method;
+
+public class ChangeStaticValue {
+    static String college="Trichandra";
+    int rol;
+String nam;
+    ChangeStaticValue(int a,String name){
+      rol=a;
+      nam=name;
+    }
+static void change(){
+    college="Ram";
+}
+void Print(){
+    System.out.println(college +"  "+nam+"  "+rol);
+}
+public static void main(String[] args) {
+ChangeStaticValue s1=new ChangeStaticValue(0, "Ram");
+s1.Print();
+ChangeStaticValue.change();
+ChangeStaticValue s2=new ChangeStaticValue(1, "Shyam");
+s2.Print();
+ChangeStaticValue s3=new ChangeStaticValue(2, "Gram");
+s3.Print();
+
+}
+
+}
---
+++ OOPs/CounterWithoutStatic.java
@@ -0,0 +1,16 @@
+package OOPs;
+
+public class CounterWithoutStatic {
+int count=0;
+CounterWithoutStatic(){
+    count++;
+}
+private void showcount(){
+    System.out.println(count);
+}
+public static void main(String[] args) {
+new CounterWithoutStatic().showcount();
+new CounterWithoutStatic().showcount();
+new CounterWithoutStatic().showcount();
+}
+}
