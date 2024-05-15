cb83872e58d0e62d2c783d8c6c4519bc1dcfe215
Snox11 <kritanshyaulak@gmail.com>
2024-05-15 19:56:12 +0545

    minor changes

--- lvl2/Matrix.java
+++ lvl2/Matrix.java
@@ -1,5 +1,5 @@
 package lvl2;
-
+import java.util.Scanner;
 public class Matrix {
     private int rows;
     private int columns;
@@ -8,5 +8,18 @@ public class Matrix {
         this.rows=rows;
         this.columns=columns;
     }
-
+    int get_rows(){
+        System.out.println("Enter the number of rows");
+        Scanner scan=new Scanner(System.in);
+        int a= scan.nextInt();
+        scan.close();
+        return a;
+    }
+    int get_columns(){
+        System.out.println("Enter the number of columns");
+        Scanner scan=new Scanner(System.in);
+        int a= scan.nextInt();
+        scan.close();
+        return a;
+    }
 }
